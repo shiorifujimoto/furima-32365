ad58265842ec995bf79b3ef2774aa6c58fb4ce52
fujifuji123412 <sena.04091919@gmail.com>
2021-02-10 14:43:46 +0900

    gimeiでfactory編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,56 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "ユーザー新規登録" do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it 'nicknameとemail、password、assword_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthdayが存在すれば登録できること' do
+      expect(@user).to be_valid
+    end
+
     it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "test@test", last_name: "")
+      @user.nickname = ''
+      @user.valid?
+      binding.pry
+      
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
-    it "emailが空だと登録できない" do
-    end
+
+    # it "emailが空だと登録できない" do
+    #   @user.nickname = ''
+    #   @user.valid?
+    #   expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    # end
+
+    # it "passwordが空だと登録できない" do
+    #   @user.nickname = ''
+    #   @user.valid?
+    #   expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    # end
+
+    # it "emailが空だと登録できない" do
+    #   @user.nickname = ''
+    #   @user.valid?
+    #   expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    # end
+
+    # it "emailが空だと登録できない" do
+    #   @user.nickname = ''
+    #   @user.valid?
+    #   expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    # end
+
+    # it "emailが空だと登録できない" do
+    #   @user.nickname = ''
+    #   @user.valid?
+    #   expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    # end
+
+    # it "emailが空だと登録できない" do
+    #   @user.nickname = ''
+    #   @user.valid?
+    #   expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    # end
   end 
 end
