4b924e96b9a3abf01c08ecdbcfb5ff3645867817
fujifuji123412 <sena.04091919@gmail.com>
2021-02-10 13:15:18 +0900

    エラーメッセージを英語表示に設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "test@test", last_name: "")
+    end
+    it "emailが空だと登録できない" do
+    end
+  end 
 end
