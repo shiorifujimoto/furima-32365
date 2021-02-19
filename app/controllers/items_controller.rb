8cb206c0d1ae22b6789fc0a7c5615c90f67d5ea3
fujifuji123412 <sena.04091919@gmail.com>
2021-02-19 17:47:40 +0900

    出品者以外のURL入力による商品編集画面遷移の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, { only: [:new, :create] }
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -27,8 +28,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
+    item = Item.find(params[:id])
+    if item.update(item_params)
       redirect_to action: :show
     else
       @item = Item.find(params[:id])
@@ -40,5 +41,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :description, :price, :category_id, :status_id, :shopping_cost_id, :prefecture_id,
                                  :shopping_day_id).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    item = Item.find(params[:id])
+    redirect_to root_path unless item.user == current_user
   end
 end
