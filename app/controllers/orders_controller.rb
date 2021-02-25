f9d3b19a32e5a36b27ac6a6eb76a19a0a8191099
fujifuji123412 <sena.04091919@gmail.com>
2021-02-25 12:07:49 +0900

    order#indexの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,22 @@
 class OrdersController < ApplicationController
   def index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
+    @order = Order.new
   end
 
+  def create
+    @order = Order.new(order_params)
+    if @order.vaild?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order)
+  end
 end
