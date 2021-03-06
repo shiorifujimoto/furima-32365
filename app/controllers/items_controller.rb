af8ce77e7246deefab28a12deaa9f4e7db98411a
fujifuji123412 <sena.04091919@gmail.com>
2021-03-06 15:14:41 +0900

    sold_out_userのアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :sold_out_user, only: [:edit]
+  before_action :sold_out_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
