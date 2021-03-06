396722eb4727ad2c997f04d2dd37ab2369faad87
fujifuji123412 <sena.04091919@gmail.com>
2021-03-06 22:28:51 +0900

    itemコントローラー　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :sold_out_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -52,10 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def correct_user
-    redirect_to root_path unless current_user == @item.user
-  end
-
-  def sold_out_user
-    redirect_to root_path unless @item.order.nil?
+    redirect_to root_path if ( current_user.id != @item.user.id ) || ( !@item.order.nil? )
   end
 end
