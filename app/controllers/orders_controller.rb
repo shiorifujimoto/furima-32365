416e30d61b32d7bc09d865d248d82e9ce1ddf3b1
fujifuji123412 <sena.04091919@gmail.com>
2021-03-08 11:25:53 +0900

    Basic認証

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create, :correct_user]
   before_action :correct_user, only: [:index, :create]
+  before_action :basic_auth
 
   def index
     @order_address = OrderAddress.new
@@ -44,4 +45,9 @@ class OrdersController < ApplicationController
     redirect_to root_path if ( current_user.id == @item.user.id ) || ( !@item.order.nil? )
   end
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BESIC_AUTH_PASSWORD"]
+    end
+  end
 end
